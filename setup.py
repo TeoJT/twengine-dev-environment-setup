721c4a30fa2bd2da9dfe3dd324fd1504f2220190
TeoJT <teojohntaylor@gmail.com>
2025-07-11 16:01:48 +0100

    upgrade to Processing 4.4.4 and new core patch

--- setup.py
+++ setup.py
@@ -6,9 +6,11 @@ import zipfile
 import shutil
 import tarfile
 
-PROCESSING_DOWNLOAD_URL_WINDOWS = 'https://github.com/processing/processing4/releases/download/processing-1295-4.3.2/processing-4.3.2-windows-x64.zip'
+debug_skip_downloading = True
 
-PROCESSING_DOWNLOAD_URL_LINUX = 'https://github.com/processing/processing4/releases/download/processing-1295-4.3.2/processing-4.3.2-linux-x64.tgz'
+PROCESSING_DOWNLOAD_URL_WINDOWS = 'https://github.com/processing/processing4/releases/download/processing-1304-4.4.4/processing-4.4.4-windows-x64-portable.zip'
+
+PROCESSING_DOWNLOAD_URL_LINUX = 'https://github.com/processing/processing4/releases/download/processing-1304-4.4.4/processing-4.4.4-linux-x64-portable.zip'
 
 
 # Prompt colors
@@ -57,89 +59,6 @@ def error(message):
         else:
             endString = len(message)
         print(message[endStringBefore+1:endString])
-
-
-# noColor = False
-# if (platform.system() == "Windows"):
-#     # No color in windows cmd
-#     HEADER    = ''
-#     YELLOW    = ''
-#     LRED      = ''
-#     BLUE      = ''
-#     PURPLE    = ''
-#     CYAN      = ''
-#     GREEN     = ''
-#     GOLD      = ''
-#     RED       = ''
-#     GREY      = ''
-#     BLACK     = ''
-#     NONE      = ''
-#     WHITE     = ''
-#     BOLD      = ''
-#     UNDERLINE = ''
-#     HRED      = ''
-#     HGREEN    = ''
-#     HYELLOW   = ''
-#     HBLUE     = ''
-#     HPURPLE   = ''
-#     HCYAN     = ''
-#     HWHITE    = ''
-
-
-CORE_DIR = "core"
-ICONS_DIR = "icons"
-LIB_DIR = "libraries"
-THEME_DIR = "theme"
-THUMB_DIR = "thumb"
-PROCESSING_DIR = "Processing"
-
-print(color.BOLD+color.PURPLE+"TWEngine Development Environment Installer"+color.NONE)
-
-# Perform initial checks
-def check_dir(dir):
-    if (not os.path.exists(dir)):
-        error("Initial check failed: missing "+dir)
-        exit(1)
-
-check_dir(CORE_DIR)
-check_dir(ICONS_DIR)
-check_dir(LIB_DIR)
-check_dir(THEME_DIR)
-check_dir(THUMB_DIR)
-
-# 1. Get install path
-print(color.CYAN+"Select install path (enter nothing for "+os.getcwd()+"):"+color.NONE)
-install_path = input()
-while (not os.path.exists(install_path)):
-    if (install_path == ""):
-        install_path = os.getcwd()
-        break
-    print(color.RED+"Path doesn't exist.")
-    print(color.CYAN+"Select install path (enter nothing for "+os.getcwd()+"):"+color.NONE)
-    install_path = input()
-
-install_path = install_path.replace("\\", "/")
-if (install_path[-1] != '/'):
-    install_path += '/'
-print(install_path)
-
-PROCESSING_DIR = install_path+PROCESSING_DIR
-
-
-# 2. Begin Processing download
-# Remove any old installations
-if (os.path.exists("processing.zip")):
-    os.remove("processing.zip")
-
-if (os.path.exists("processing.tgz")):
-    os.remove("processing.tgz")
-
-if (os.path.exists(PROCESSING_DIR)):
-    print("Deleting old dir...")
-    shutil.rmtree(PROCESSING_DIR)
-
-# TODO: Check for newer Timeway version via the github download page.
-
 
 def download_file(url, destination):
     # Open the URL
@@ -217,9 +136,93 @@ def is_wsl():
     return False
 
 
+# noColor = False
+# if (platform.system() == "Windows"):
+#     # No color in windows cmd
+#     HEADER    = ''
+#     YELLOW    = ''
+#     LRED      = ''
+#     BLUE      = ''
+#     PURPLE    = ''
+#     CYAN      = ''
+#     GREEN     = ''
+#     GOLD      = ''
+#     RED       = ''
+#     GREY      = ''
+#     BLACK     = ''
+#     NONE      = ''
+#     WHITE     = ''
+#     BOLD      = ''
+#     UNDERLINE = ''
+#     HRED      = ''
+#     HGREEN    = ''
+#     HYELLOW   = ''
+#     HBLUE     = ''
+#     HPURPLE   = ''
+#     HCYAN     = ''
+#     HWHITE    = ''
+
+
+CORE_DIR = "core"
+ICONS_DIR = "icons"
+LIB_DIR = "libraries"
+THEME_DIR = "theme"
+THUMB_DIR = "thumb"
+PROCESSING_DIR = "Processing"
+
+print(color.BOLD+color.PURPLE+"TWEngine Development Environment Installer"+color.NONE)
+
+# Perform initial checks
+def check_dir(dir):
+    if (not os.path.exists(dir)):
+        error("Initial check failed: missing "+dir)
+        exit(1)
+
+check_dir(CORE_DIR)
+check_dir(ICONS_DIR)
+check_dir(LIB_DIR)
+check_dir(THEME_DIR)
+# check_dir(THUMB_DIR)
+
+# TODO: Timeway now installs at a set path with the MSI installers. There is no need for this path selection code.
+# 1. Get install path
+print(color.CYAN+"Select install path (enter nothing for "+os.getcwd()+"):"+color.NONE)
+install_path = input()
+while (not os.path.exists(install_path)):
+    if (install_path == ""):
+        install_path = os.getcwd()
+        break
+    print(color.RED+"Path doesn't exist.")
+    print(color.CYAN+"Select install path (enter nothing for "+os.getcwd()+"):"+color.NONE)
+    install_path = input()
+
+install_path = install_path.replace("\\", "/")
+if (install_path[-1] != '/'):
+    install_path += '/'
+print(install_path)
+
+
+PROCESSING_DIR = install_path+PROCESSING_DIR
+
+
+# 2. Begin Processing download
+# Remove any old installations
+if (os.path.exists("processing.zip")):
+    os.remove("processing.zip")
+
+if (os.path.exists("processing.tgz")):
+    os.remove("processing.tgz")
+
+if (os.path.exists(PROCESSING_DIR) and not debug_skip_downloading):
+    print("Deleting old dir...")
+    shutil.rmtree(PROCESSING_DIR)
+
+# TODO: Check for newer Timeway version via the github download page.
+
 
 # Install processing
-if (True):
+# CHANGE: Processing now compresses linux downloads with ZIP, not TGZ.
+if (not debug_skip_downloading):
     print(color.CYAN+"Install Processing"+color.NONE)
     try:
         if (os.name == "nt"):
@@ -227,7 +230,7 @@ if (True):
         elif (os.name == "posix" and is_wsl()):
             download_file(PROCESSING_DOWNLOAD_URL_WINDOWS, install_path+'processing.zip')
         elif (os.name == "posix" and not is_wsl()):
-            download_file(PROCESSING_DOWNLOAD_URL_LINUX, install_path+'processing.tgz')
+            download_file(PROCESSING_DOWNLOAD_URL_LINUX, install_path+'processing.zip')
         else:
             error("Unknown operating system (os.name: "+os.name+", is_wsl: "+str(is_wsl())+")")
             exit(1)
@@ -239,25 +242,29 @@ if (True):
         error("Unknown download error.")
         exit(1)
 
+    # No need to check system since all downloads are now TGZ
     try:
-        if (os.name == "nt"):
-            extract_zip(install_path+'processing.zip', PROCESSING_DIR)
-        elif (os.name == "posix" and is_wsl()):
-            extract_zip(install_path+'processing.zip', PROCESSING_DIR)
-        elif (os.name == "posix" and not is_wsl()):
-            extract_tgz(install_path+'processing.tgz', PROCESSING_DIR)
-        else:
-            error("Unknown operating system (os.name: "+os.name+", is_wsl: "+str(is_wsl())+")")
-            exit(1)
+        # if (os.name == "nt"):
+        #     extract_zip(install_path+'processing.zip', PROCESSING_DIR)
+        # elif (os.name == "posix" and is_wsl()):
+        #     extract_zip(install_path+'processing.zip', PROCESSING_DIR)
+        # elif (os.name == "posix" and not is_wsl()):
+        #     extract_tgz(install_path+'processing.tgz', PROCESSING_DIR)
+        # else:
+        #     error("Unknown operating system (os.name: "+os.name+", is_wsl: "+str(is_wsl())+")")
+        #     exit(1)
 
+        extract_zip(install_path+'processing.zip', PROCESSING_DIR)
     except:
-        error("ZIP extract error, perhaps zip file is corrupted?")
+        error("Extract error, perhaps file is corrupted?")
         exit(1)
 
 
-# Delete zip
+# Delete zip/tgz
 if (os.path.exists("processing.zip")):
     os.remove(install_path+"processing.zip")
+if (os.path.exists("processing.tgz")):
+    os.remove(install_path+"processing.tgz")
 
 # Have a peek inside and update PROCESSING_DIR
 zip_contents = os.listdir(PROCESSING_DIR)
@@ -271,13 +278,13 @@ print(PROCESSING_DIR)
 # Stage 3:
 # Replace core with patch
 print("Patching core...")
-if (os.path.exists(PROCESSING_DIR+"core")):
-    shutil.rmtree(PROCESSING_DIR+"core")
+if (os.path.exists(PROCESSING_DIR+"app/resources/core")):
+    shutil.rmtree(PROCESSING_DIR+"app/resources/core")
 else:
     print(color.GOLD+"Warning: original core appears to be missing. This may indicate a corrupted download. This isn't an issue yet but further issues may arise."+color.NONE)
 
 # Copy core patch
-shutil.copytree(CORE_DIR, PROCESSING_DIR+"core")
+shutil.copytree(CORE_DIR, PROCESSING_DIR+"app/resources/core")
 
 # Stage 4:
 # Icons install
@@ -287,24 +294,24 @@ def copy_from(src, dst):
         shutil.copyfile(src+"/"+file, dst+"/"+file)
 
 print("Installing Timeway icons...")
-if (os.path.exists(PROCESSING_DIR+"modes/java/application")):
-    copy_from(ICONS_DIR, PROCESSING_DIR+"modes/java/application")
+if (os.path.exists(PROCESSING_DIR+"app/resources/modes/java/application")):
+    copy_from(ICONS_DIR, PROCESSING_DIR+"app/resources/modes/java/application")
 else:
-    print(color.GOLD+"Warning: "+PROCESSING_DIR+"modes/java/application not found. This may indicate a corrupted install."+color.NONE)
+    print(color.GOLD+"Warning: "+PROCESSING_DIR+"app/resources/modes/java/application not found. This may indicate a corrupted install."+color.NONE)
 
 
 # Stage 5:
 # Theme and thumb install
 print("Installing theme...")
-if (os.path.exists(PROCESSING_DIR+"lib/theme")):
+if (os.path.exists(PROCESSING_DIR+"app/resources/lib/theme")):
     # Copy theme
-    shutil.copyfile(THEME_DIR+"/theme.txt", PROCESSING_DIR+"lib/theme/theme.txt")
+    shutil.copyfile(THEME_DIR+"/theme.txt", PROCESSING_DIR+"app/resources/lib/theme/theme.txt")
     # Copy default theme
-    if (os.path.exists(PROCESSING_DIR+"lib/theme/Alloys/imilac.txt")):
-        shutil.copyfile(THEME_DIR+"/theme.txt", PROCESSING_DIR+"lib/theme/Alloys/imilac.txt")
+    if (os.path.exists(PROCESSING_DIR+"app/resources/lib/theme/Alloys/imilac.txt")):
+        shutil.copyfile(THEME_DIR+"/theme.txt", PROCESSING_DIR+"app/resources/lib/theme/Alloys/imilac.txt")
     # Eh can't be bothered with warnings for this part.
 else:
-    print(color.GOLD+"Warning: "+PROCESSING_DIR+"lib/theme not found. This may indicate a corrupted install."+color.NONE)
+    print(color.GOLD+"Warning: "+PROCESSING_DIR+"app/resources/lib/theme not found. This may indicate a corrupted install."+color.NONE)
 
 # TODO: Thumbnail
 
